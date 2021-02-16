e07d0b75288d9143b8cc6bdc1fbb2acf9e08de35
Kexin Xie <kexin.xie@salesforce.com>
2021-02-15 17:08:44 -0800

    First simple implementation

---
+++ project/plugins.sbt
@@ -0,0 +1,1 @@
+addSbtPlugin("com.salesforce.mce" % "sbt-mce-champloo" % "[0.22.2,)")
---
+++ src/main/scala/com/salesforce/mce/telepathy/HttpRequest.scala
@@ -0,0 +1,51 @@
+package com.salesforce.mce.telepathy
+
+import io.circe.parser.decode
+import io.circe.syntax._
+import io.circe.{Decoder, Encoder}
+
+import okhttp3.{HttpUrl, MediaType, Request, RequestBody}
+
+object HttpRequest {
+
+  final val JsonContentType = MediaType.get("application/json; charset=utf-8")
+
+  def request[Rsp: Decoder](url: HttpUrl, request: Request)(implicit
+    setting: TelepathySetting
+  ): Either[Exception, Rsp] = {
+    val response = setting.buildClient().newCall(request).execute()
+
+    if (response.isSuccessful())
+      decode[Rsp](response.body().string())
+    else
+      Left(
+        new RuntimeException(s"Response not OK (${response.code()}, error ${response.message()}")
+      )
+  }
+
+  def get[Rsp: Decoder](url: HttpUrl)(implicit setting: TelepathySetting): Either[Exception, Rsp] =
+    request(url, setting.requestBuilder(url).build())
+
+  def put[Rsp: Decoder, D: Encoder](url: HttpUrl, data: D)(implicit
+    setting: TelepathySetting
+  ): Either[Exception, Rsp] =
+    request(
+      url,
+      setting
+        .requestBuilder(url)
+        .put(RequestBody.create(data.asJson.noSpaces, JsonContentType))
+        .build()
+    )
+
+  def post[Rsp: Decoder, D: Encoder](url: HttpUrl, data: D)(implicit
+    setting: TelepathySetting
+  ): Either[Exception, Rsp] =
+    request(
+      url,
+      setting
+        .requestBuilder(url)
+        .post(RequestBody.create(data.asJson.noSpaces, JsonContentType))
+        .build()
+    )
+
+}
